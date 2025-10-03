65837c156230f6e1f6deeb9e859edd5aa54d7189
damianmoralesreyes315-beep <damianmoralesreyes315@gmail.com>
2025-10-02 19:15:07 -0700

    Update script.js
    Versión 1

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function tresImagenes() {
   document.getElementById("boton").innerHTML = "imágenes de alta calidad";
 }
+
+function pruebaOne() {
+  el =
+document.querySelector("#prueba2"); el.innerHTML = "imágenes";
+}
+
+function imagenAuto() {
+const el =
+  document.getElementsByTagName("li"); const p =
+  document.querySelector("#id-5");
+  p.innerText = el [2].innerHTML;
+}
+  
+  function register() {
+  const paragraph = document.querySelector("#message");
+  const username = document.getElementById("usernameInput").value;
+    paragraph.innerHTML = username + " : completo"; 
+  }
